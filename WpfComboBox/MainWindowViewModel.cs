d6955262a27b5b9b7581713680e75d6537841663
K0GiHF9S <kidoairakusyunkasyuutou@gmail.com>
2022-10-28 20:51:21 +0900

    StoreLastSelectBehavior

---
+++ WpfComboBox/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfComboBox;
+
+internal class MainWindowViewModel : INotifyPropertyChanged
+{
+    public ObservableCollection<string> Names { get; }
+
+    private string appendName = String.Empty;
+
+    public string AppendName
+    {
+        get => appendName;
+        set
+        {
+            appendName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string selectedName = String.Empty;
+
+    public string SelectedName
+    {
+        get => selectedName;
+        set
+        {
+            selectedName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public RelayCommand AppendCommand { get; }
+    public RelayCommand DeleteCommand { get; }
+    public RelayCommand RefleshCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        Names = new() { "a", "b", "c" };
+        AppendCommand = new(() => Names.Add(AppendName));
+        DeleteCommand = new(() => Names.Remove(SelectedName));
+        RefleshCommand = new(() =>
+        {
+            var backup = Names.ToArray();
+            Names.Clear();
+            foreach (var item in backup)
+            {
+                Names.Add(item);
+            }
+        });
+        PropertyChanged += MainWindowViewModel_PropertyChanged;
+    }
+
+    private void MainWindowViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
---
+++ WpfComboBox/StoreLastSelectBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows;
+using Microsoft.Xaml.Behaviors;
+using System;
+
+namespace WpfComboBox;
+
+internal class StoreLastSelectBehavior : Behavior<ComboBox>
+{
+    private WeakReference<object?> _lastSelected = new(null);
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        if (AssociatedObject.ItemsSource is INotifyCollectionChanged changed)
+        {
+            AssociatedObject.SelectionChanged += SelectionChanged;
+            changed.CollectionChanged += CollectionChanged;
+        }
+    }
+
+    protected override void OnDetaching()
+    {
+        if (AssociatedObject.ItemsSource is INotifyCollectionChanged changed)
+        {
+            changed.CollectionChanged -= CollectionChanged;
+            AssociatedObject.SelectionChanged -= SelectionChanged;
+        }
+        base.OnDetaching();
+    }
+
+    private void CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (sender is IEnumerable<object> collection
+            && _lastSelected.TryGetTarget(out var last_selected)
+            && collection.FirstOrDefault(o => o.Equals(last_selected)) is object item)
+        {
+            AssociatedObject.SelectedItem = item;
+        }
+    }
+
+    private void SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.AddedItems.Count == 1)
+        {
+            _lastSelected.SetTarget(e.AddedItems[0]);
+        }
+    }
+}
